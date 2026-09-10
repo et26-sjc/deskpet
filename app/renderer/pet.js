404b5ba5cd626b759cee7a07244a87f26ad236a4
Jingchao She <163161756+et26-sjc@users.noreply.github.com>
2026-09-10 11:12:36 +0800

    Integrate companion interaction messages

--- app/renderer/pet.js
+++ app/renderer/pet.js
@@ -6,8 +6,27 @@ const actions = {
   sleep: '../../assets/character/sleep.png'
 };
 
+const messages = [
+  '你好呀',
+  '今天也辛苦啦',
+  '记得休息一下'
+];
+
 function play(action) {
   if (actions[action]) sprite.src = actions[action];
+}
+
+function speak() {
+  const bubble = document.createElement('div');
+  bubble.innerText = messages[Math.floor(Math.random() * messages.length)];
+  bubble.style.position = 'absolute';
+  bubble.style.top = '10px';
+  bubble.style.left = '10px';
+  bubble.style.background = 'white';
+  bubble.style.padding = '8px';
+  bubble.style.borderRadius = '10px';
+  document.body.appendChild(bubble);
+  setTimeout(() => bubble.remove(), 2500);
 }
 
 setInterval(() => {
@@ -17,5 +36,6 @@ setInterval(() => {
 
 window.addEventListener('click', () => {
   play('wave');
+  speak();
   setTimeout(() => play('idle'), 2000);
 });
